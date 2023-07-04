1994e34859eefbf17c6863251f6932a97f4be0a3
MerakiFC <pandaxsunshine@gmail.com>
2023-07-04 21:16:01 +1000

    Add print parameters and correct envFile path

--- WebhookScripts/ReceiverCode/apiConn.py
+++ WebhookScripts/ReceiverCode/apiConn.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-envFile = "../devEnv/"
+envFile = "../devEnv/FCMToken.env"
 
 load_dotenv(dotenv_path=envFile)
 
-print(os.getenv("FCM_ORG_ID"))
+print("ORG ID = ", os.getenv("FCM_ORG_ID"))
+print("Webex API URL: ", os.getenv("WX_API_URL"))
