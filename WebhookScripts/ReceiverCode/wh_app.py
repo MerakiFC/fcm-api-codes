a04f5f2d1b982d28c3779b81c69aa943146092e6
pandaxsunshine@gmail.com <pandaxsunshine@gmail.com>
2023-07-17 18:06:37 +1000

    Minor cosmetic update to wh_app Webhook received message -- addded \n

--- WebhookScripts/ReceiverCode/wh_app.py
+++ WebhookScripts/ReceiverCode/wh_app.py
@@ -23,7 +23,7 @@ def mv_task():
     
     dictWhPayload = request.get_json()  # Get the JSON payload from the request
     strTimestampAEST = str(epochToAest(dictWhPayload["alertData"]["timestamp"]))
-    print("---\n---\nWebhook timestamp: ",strTimestampAEST,"\nStarting process...---\n---")
+    print("---\n---\nWebhook timestamp: ",strTimestampAEST,"\nStarting process...\n---\n---")
 
 
     ##Process the payload and perform necessary actions
